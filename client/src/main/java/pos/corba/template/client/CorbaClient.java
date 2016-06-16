f8de405a2b8d343acb44dbcf863c72384b2116bd
Marcus Vinicius <vmvini@hotmail.com>
2016-06-16 10:57:42 -0300

    simplest abstraction with java reflection

--- client/src/main/java/pos/corba/template/client/CorbaClient.java
+++ client/src/main/java/pos/corba/template/client/CorbaClient.java
@@ -6,6 +6,9 @@
 package pos.corba.template.client;
 
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CORBA.*;
 import org.omg.CosNaming.NamingContextPackage.CannotProceed;
@@ -20,14 +23,11 @@ import pos.corba.template.addressconfig.Address;
  */
 public abstract  class CorbaClient<T> {
     
+    protected abstract String getResourceName();
     
     protected abstract void doBusinessLogic(T remoteObject);
     
-    protected abstract T getRemoteObject(NamingContextExt ncRef, String resourceName) throws NotFound, CannotProceed, InvalidName;
-    
-    protected abstract String getResourceName();
-    
-    public void doCorbaThings() throws InvalidName, NotFound, CannotProceed, org.omg.CORBA.ORBPackage.InvalidName{
+    public void doCorbaThings() throws InvalidName, NotFound, CannotProceed, org.omg.CORBA.ORBPackage.InvalidName, NoSuchMethodException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         Address address = new Address();
         ORB orb = ORB.init(address.getConnectionArgs(), System.getProperties());
         org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
@@ -36,5 +36,32 @@ public abstract  class CorbaClient<T> {
         T remoteObject = getRemoteObject(ncRef, name);
         doBusinessLogic(remoteObject);
     }
+ 
+    private T getRemoteObject(NamingContextExt ncRef, String resourceName) throws NotFound, CannotProceed, InvalidName, NoSuchMethodException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+        java.lang.Object o = invokeNarrow(getGenericTypeClassName() + "Helper", ncRef.resolve_str(resourceName));
+        return (T)o;
+    }
+    
+    private  Class<T> getClassByGenericType(){
+       return (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+    
+    private String getGenericTypeClassName(){
+        return getClassByGenericType().getName();
+    }
+    
+    private Method getMethod(String className, String methodName, Class parameterType) throws ClassNotFoundException, NoSuchMethodException{
+        Class myclass = Class.forName(className);
+        Method method = myclass.getMethod(methodName, parameterType);
+        return method;
+    }
+    
+    private java.lang.Object invokeNarrow(String helperName, org.omg.CORBA.Object o ) throws NoSuchMethodException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+        Method narrow = getMethod(helperName, "narrow", org.omg.CORBA.Object.class);
+        java.lang.Object resp = narrow.invoke(null, o);
+        return resp;
+    }
+    
+    
     
 }
