d3242f7237479f8768805cb6db7f99fbcbb3ed01
Marcus Vinicius <vmvini@hotmail.com>
2016-06-19 15:27:27 -0300

    using Job's Service-Locator API on CorbaClient

--- client/src/main/java/pos/corba/template/client/CorbaClient.java
+++ client/src/main/java/pos/corba/template/client/CorbaClient.java
@@ -6,62 +6,49 @@
 package pos.corba.template.client;
 
 
+import ifpb.locator.ServerContext;
+import ifpb.locator.context.IIOPContext;
+import ifpb.locator.named.App;
+import ifpb.locator.named.Scoped;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import org.omg.CosNaming.NamingContextExt;
-import org.omg.CORBA.*;
-import org.omg.CosNaming.NamingContextPackage.CannotProceed;
-import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
-import pos.corba.template.addressconfig.Address;
 
 
 /**
  *
  * @author vmvini
  */
-public abstract  class CorbaClient<T> {
+public class CorbaClient{
     
-    protected abstract String getResourceName();
     
-    protected abstract void doBusinessLogic(T remoteObject);
-    
-    public void doCorbaThings() throws InvalidName, NotFound, CannotProceed, org.omg.CORBA.ORBPackage.InvalidName, NoSuchMethodException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
-        Address address = new Address();
-        ORB orb = ORB.init(address.getConnectionArgs(), System.getProperties());
-        org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-        NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-        String name = getResourceName();
-        T remoteObject = getRemoteObject(ncRef, name);
-        doBusinessLogic(remoteObject);
+    /**
+     * Use esse método para fazer lookup em um objeto remoto através de CORBA
+     * @param <T> Tipo específico do objeto remoto que você quer recuperar
+     * @param resourceName Nome usado para localizar o objeto remoto
+     * @param remoteObjectClassType Classe do objeto remoto que você quer recuperar
+     * @return
+     * @throws ClassNotFoundException
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException 
+     */
+    public static <T> T getRemoteObject( String resourceName, Class<T> remoteObjectClassType ) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+        
+        //instanciando contexto passando o ConfigContext IIOPContext como argumento
+        ServerContext contexto = new ServerContext(new IIOPContext(),  App.name(null).namespace(Scoped.EMPTY));
+        org.omg.CORBA.Object obj =  contexto.bean(resourceName).locate();
+        
+        //Acessando classe *Helper até então desconhecida
+        String helperName = remoteObjectClassType.getCanonicalName() + "Helper";
+        Class myclass = Class.forName(helperName);
+        
+        //Chamando o metódo narrow da classe *Helper
+        Method method = myclass.getMethod("narrow", org.omg.CORBA.Object.class);
+        java.lang.Object remoteObj = method.invoke(null, obj);
+        
+        //retornando objeto remoto com o tipo específico
+        return (T)remoteObj;
     }
- 
-    private T getRemoteObject(NamingContextExt ncRef, String resourceName) throws NotFound, CannotProceed, InvalidName, NoSuchMethodException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
-        java.lang.Object o = invokeNarrow(getGenericTypeClassName() + "Helper", ncRef.resolve_str(resourceName));
-        return (T)o;
-    }
-    
-    private  Class<T> getClassByGenericType(){
-       return (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-    }
-    
-    private String getGenericTypeClassName(){
-        return getClassByGenericType().getName();
-    }
-    
-    private Method getMethod(String className, String methodName, Class parameterType) throws ClassNotFoundException, NoSuchMethodException{
-        Class myclass = Class.forName(className);
-        Method method = myclass.getMethod(methodName, parameterType);
-        return method;
-    }
-    
-    private java.lang.Object invokeNarrow(String helperName, org.omg.CORBA.Object o ) throws NoSuchMethodException, ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
-        Method narrow = getMethod(helperName, "narrow", org.omg.CORBA.Object.class);
-        java.lang.Object resp = narrow.invoke(null, o);
-        return resp;
-    }
-    
-    
-    
+
 }
